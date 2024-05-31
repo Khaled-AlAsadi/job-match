663dc74e269c2a806783ee58566527b049f59204
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-05-31 11:07:19 +0200

    Create django-app

---
+++ job_match_backend/job_match_backend_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(['GET'])
+def index(request):
+    return HttpResponse("Använd rätt route för att hitta saker")
