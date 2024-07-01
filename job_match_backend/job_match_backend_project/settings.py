7b1e6dddadb58f5fc73c2c1bd9f1f99e22cf985c
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-07-01 15:17:33 +0200

    Add simplejwt to handle auth in frontend

--- job_match_backend/job_match_backend_project/settings.py
+++ job_match_backend/job_match_backend_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,12 +39,25 @@ if RENDER_EXTERNAL_HOSTNAME:
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
+    'rest_framework',
+    'rest_framework_simplejwt',
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "job_match_backend_app"
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- job_match_backend/job_match_backend_project/urls.py
+++ job_match_backend/job_match_backend_project/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("job_match_backend_app.urls"))
+    path("", include("job_match_backend_app.urls")),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('refresh/token', TokenRefreshView.as_view(), name='token_refresh'),
 ]
