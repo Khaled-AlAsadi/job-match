c8a5b0bead18bef63f725bc146c0e5b6f78a6d05
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-07-08 01:07:40 +0200

    New updates

--- job_match_frontend/src/pages/EmployeePage.tsx
+++ job_match_frontend/src/pages/EmployeePage.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/authContext";
+import { retrieveAvailableJobPosts } from "../services/employeeService";
+
 
 const EmployeePage = () => {
-  const { user } = useAuth();
+  const [jobPosts, setJobPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { user, authTokens } = useAuth();
+ 
+  useEffect(() => {
+    const fetchJobPosts = async () => {
+      try {
+        const data = await retrieveAvailableJobPosts(authTokens?.access);
+        setJobPosts(data);
+      } catch (error:any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchJobPosts();
+  }, [user]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
   return (
-    <p>Welcome EmployeePage, {user?.email}!</p>
-
+    <div>
+      <p>Welcome EmployeePage, {user?.email}!</p>
+      <h2>Available Job Posts</h2>
+      <ul>
+        {jobPosts.map((job:any) => (
+          <li key={job.id}>{job.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
