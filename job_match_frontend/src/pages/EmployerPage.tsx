b4f9425c12f703dd15dc6b104fd9816cd9821ced
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-07-10 12:00:09 +0200

    Add service for employer

--- job_match_frontend/src/pages/EmployerPage.tsx
+++ job_match_frontend/src/pages/EmployerPage.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/authContext";
+import { retrieveEmployerJobPosts } from "../services/employerService";
 
 const EmployerPage = () => {
-  const { user } = useAuth();
+  const [jobPosts, setJobPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const { user, authTokens } = useAuth();
+
+  useEffect(() => {
+    const fetchEmployerJobPosts = async () => {
+      if (user?.is_ag === true) {
+        try {
+          const data = await retrieveEmployerJobPosts(authTokens?.access);
+          setJobPosts(data);
+        } catch (error: any) {
+          setError(error.message);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchEmployerJobPosts();
+  }, [user, authTokens]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
 
   return (
     <p>Welcome EmployerPage, {user?.email}!</p>
