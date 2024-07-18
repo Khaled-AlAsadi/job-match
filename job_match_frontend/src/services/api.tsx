659ae9c3d8a1563e9e85def8ed2361547f02acbd
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-07-18 12:56:07 +0200

    Add env production file for base url

--- job_match_frontend/src/services/api.tsx
+++ job_match_frontend/src/services/api.tsx
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_BASE_URL_PROD
+    : 'http://127.0.0.1:8000'
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
