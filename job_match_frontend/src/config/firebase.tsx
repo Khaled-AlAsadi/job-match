8c9af08d9d723c4e9c2f7e7ba72d3a4785cb3a1a
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-07-18 10:25:02 +0200

    Trying firebase for hosting

---
+++ job_match_frontend/src/config/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAYGZXAqfa2Yh4mBGSS6m_XLadBWGyrWFw',
+  authDomain: 'job-match-web-32db5.firebaseapp.com',
+  projectId: 'job-match-web-32db5',
+  storageBucket: 'job-match-web-32db5.appspot.com',
+  messagingSenderId: '958649555189',
+  appId: '1:958649555189:web:2b3945a257b2f81e4d8dbc',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
