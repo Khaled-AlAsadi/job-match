eee0f55f9a5ca83a4916a830c9aa42d8219031be
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-07-20 14:42:49 +0200

    Remove unused variable

--- job_match_frontend/src/services/api.tsx
+++ job_match_frontend/src/services/api.tsx
@@ -1,10 +1,5 @@
 import axios from 'axios'
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_BASE_URL_PROD
-    : 'http://127.0.0.1:8000'
-
 const api = axios.create({
   baseURL: 'https://job-match-fg8r.onrender.com',
   headers: {
