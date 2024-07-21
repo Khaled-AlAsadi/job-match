d3bc17bb53eb87248602a330b33c45c39fafefcb
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-07-21 17:11:00 +0200

    Add profile component

--- job_match_frontend/src/routes/AppRoutes.tsx
+++ job_match_frontend/src/routes/AppRoutes.tsx
@@ -11,6 +11,7 @@ import HomePage from '../pages/HomePage'
 import PrivateRoute from './PrivateRoute'
 import JobPage from '../pages/JobPage'
 import SignupPage from '../pages/SignupPage'
+import ProfilePage from '../pages/ProfilePage'
 
 const AppRoutes: React.FC = () => {
   return (
@@ -22,6 +23,7 @@ const AppRoutes: React.FC = () => {
         <Route element={<PrivateRoute />}>
           <Route path="/home" element={<HomePage />} />
           <Route path="/employer/job/:id" element={<JobPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Route>
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
