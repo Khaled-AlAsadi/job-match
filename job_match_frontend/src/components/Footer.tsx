291df3e396d4fb033aee219351397b71594e24bf
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-07-23 11:56:35 +0200

    Change footer style

--- job_match_frontend/src/components/Footer.tsx
+++ job_match_frontend/src/components/Footer.tsx
@@ -1,19 +1,27 @@
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import styled from 'styled-components'
 
 function FooterComponent() {
   return (
     <Footer>
-      <div>
-        <a
+      <Container>
+        <StyledLink
           href="https://github.com/Khaled-AlAsadi"
           target="_blank"
           rel="noopener noreferrer"
-          aria-label="View our GitHub profile"
+          aria-label="View GitHub profile"
         >
           <FaGithub />
-        </a>
-      </div>
+        </StyledLink>
+        <StyledLink
+          href="https://www.linkedin.com/in/khaled-al-asadi-a0a169193/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="View LinkedIn profile"
+        >
+          <FaLinkedin />
+        </StyledLink>
+      </Container>
     </Footer>
   )
 }
@@ -21,11 +29,32 @@ function FooterComponent() {
 const Footer = styled.footer`
   background-color: #333;
   color: white;
-  padding: 10px;
+  padding: 20px;
   text-align: center;
   position: relative;
   width: 100%;
   bottom: 0;
   left: 0;
+  box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.2);
+`
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`
+const StyledLink = styled.a`
+  color: white;
+  text-decoration: none;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 48px;
+
+  &:hover {
+    color: #e1e1e1;
+    background-color: #444;
+    border-radius: 50%;
+    padding: 10px;
+  }
 `
 export default FooterComponent
