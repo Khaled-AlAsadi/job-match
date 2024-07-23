158b724aa926e5f703442e838f3e8444aa9f3b06
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-07-23 08:31:58 +0200

    Update

--- job_match_frontend/src/pages/HomePage.tsx
+++ job_match_frontend/src/pages/HomePage.tsx
@@ -1,15 +1,17 @@
+import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/authContext'
 import EmployeePage from './EmployeePage'
 import EmployerPage from './EmployerPage'
 
 const HomePage = () => {
   const { user } = useAuth()
+  const navigate = useNavigate()
 
   if (!user) {
-    return <p>Loading...</p>
+    navigate('/login')
+  } else {
+    return user.is_ag ? <EmployerPage /> : <EmployeePage />
   }
-
-  return user.is_ag ? <EmployerPage /> : <EmployeePage />
 }
 
 export default HomePage
