10af9c2209541e72b3f611e581ed301c1d3c7c85
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-07-23 13:21:52 +0200

    Hide sensitive data

--- job_match_frontend/src/config/firebase.tsx
+++ job_match_frontend/src/config/firebase.tsx
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAYGZXAqfa2Yh4mBGSS6m_XLadBWGyrWFw',
-  authDomain: 'job-match-web-32db5.firebaseapp.com',
-  projectId: 'job-match-web-32db5',
-  storageBucket: 'job-match-web-32db5.appspot.com',
-  messagingSenderId: '958649555189',
-  appId: '1:958649555189:web:2b3945a257b2f81e4d8dbc',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
 }
 
 // Initialize Firebase
